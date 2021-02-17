b0d88e1038d1a6d02f669f77908a79e8abb72a00
thiernoabarry <thierno.abarry@yahoo.fr>
2021-02-17 16:34:20 +0100

    Completed module

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -16,15 +16,19 @@ import java.util.Optional;
 public class BlogController {
 
     private PostRepository postRepository;
+    private CategoryRepository categoryRepository;
 
-    public BlogController(PostRepository postRepository) {
+    public BlogController(PostRepository postRepository, CategoryRepository categoryRepository) {
         this.postRepository = postRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         List<Post> posts = postRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
         modelMap.put("posts", posts);
+        modelMap.put("categories", categories);
         return "home";
     }
 
@@ -33,5 +37,26 @@ public class BlogController {
         Post post = postRepository.findById(id).orElse(null);
         modelMap.put("post", post);
         return "post-details";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String categoryList(@PathVariable Long id, ModelMap modelMap){
+        Category category = categoryRepository.findById(id).orElse(null);
+        modelMap.put("category", category);
+        return "category-details";
+    }
+
+    @RequestMapping("/posts")
+    public String postList(@PathVariable Category category, ModelMap modelMap){
+        List<Post> posts = postRepository.findByCategory(category);
+        modelMap.put("posts",posts);
+        return "posts-categorie";
+    }
+
+    @RequestMapping("/categories")
+    public String categoryList(ModelMap modelMap){
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.put("categories",categories);
+        return "categories";
     }
 }
